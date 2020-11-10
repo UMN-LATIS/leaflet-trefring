057e24efacd8a2b40080dc1a4584a753400d5f41
Colin McFadden <cmcfadden@gmail.com>
2020-11-10 16:47:55 -0600

    remove power of two scaling stuff

--- Leaflet.TileLayer.GL.js
+++ Leaflet.TileLayer.GL.js
@@ -345,35 +345,15 @@ L.TileLayer.GL = L.GridLayer.extend({
 
 		gl.activeTexture(gl.TEXTURE0 + index);
 		gl.bindTexture(gl.TEXTURE_2D, this._textures[index]);
-		if (!this.isPowerOfTwo(imageData.width) || !this.isPowerOfTwo(imageData.height)) {
-			// Scale up the texture to the next highest power of two dimensions.
-			var canvas = document.createElement("canvas");
-			canvas.width = this.nextHighestPowerOfTwo(imageData.width);
-			canvas.height = this.nextHighestPowerOfTwo(imageData.height);
-			var ctx = canvas.getContext("2d");
-			ctx.drawImage(imageData, 0, 0, imageData.width, imageData.height);
-			imageData = canvas;
-		}
-		gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, imageData);
-		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_NEAREST);
-		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+
+		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
 		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
 		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-		gl.generateMipmap(gl.TEXTURE_2D);
+		gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, imageData);
+
 	},
 
 
-isPowerOfTwo: function (x) {
-    return (x & (x - 1)) == 0;
-},
-
-nextHighestPowerOfTwo: function(x) {
-    --x;
-    for (var i = 1; i < 32; i <<= 1) {
-        x = x | x >> i;
-    }
-    return x + 1;
-},
 
 	// Gets called by L.GridLayer before createTile(), just before coord wrapping happens.
 	// Needed to store the context of each <canvas> tile when the tile coords is wrapping.
