dd4bdefd4a2e259f6b2e4fe4a6c2940ea9eb3ae3
thorn573 <thorn573@umn.edu>
2021-06-28 14:18:25 -0500

    Median is separate function now.

--- _site/leaflet-treering.js
+++ _site/leaflet-treering.js
@@ -2995,6 +2995,73 @@ function Popout(Lt) {
 
   };
 
+  PopoutPlots.prototype.median = function (shapes, colors, data) {
+    // auto spaghetti plot
+    // 1) find median width (across all data) per year
+    var longestDataLength = 0;
+    for (set of data) {
+      var setLength = set.widths.length;
+      if (setLength > longestDataLength) {
+        longestDataLength = setLength;
+      }
+    }
+
+    var medianYears = [];
+    var medianWidths = [];
+    var year_to_find_median_width_for = Number.MAX_SAFE_INTEGER;
+    var last_year_to_account_for = -1 * (Number.MAX_SAFE_INTEGER);
+
+    for (set of data) {
+      let years = set.years.filter(Boolean);
+
+      var sets_first_year = parseInt(years[0])
+      if (sets_first_year < year_to_find_median_width_for) { // find oldest (smallest) year in all sets of data
+        year_to_find_median_width_for = sets_first_year;
+      }
+
+      var sets_last_year = parseInt(years[years.length - 1])
+      if (sets_last_year > last_year_to_account_for) { // find most recent (largest) year in all sets of data
+        last_year_to_account_for = sets_last_year;
+      }
+    }
+
+    while (year_to_find_median_width_for <= last_year_to_account_for) {
+      var single_year_widths = []
+      for (set of data) { // loop through data sets
+        for (let i = 0; i < set.years.length; i++) { // loop through years & widths of each data set
+          if ((set.years.length > 0) && (parseInt(set.years[i]) == year_to_find_median_width_for)) {
+            single_year_widths.push(parseFloat(set.widths[i]));
+          }
+        }
+      }
+
+      single_year_widths.sort( (a, b) => { return a - b} ); // sort into asscending order
+      if (single_year_widths.length == 0) {
+        medianWidths.push('0');
+      } else if (single_year_widths.length % 2 == 0) { // if even length, need to take average of middle values
+        var midUpper = (single_year_widths.length / 2);
+        var midLower = (single_year_widths.length / 2) - 1;
+        var value = String((single_year_widths[midUpper] + single_year_widths[midLower]) / 2);
+        medianWidths.push(value);
+      } else {
+        var mid = Math.floor(single_year_widths.length / 2);
+        medianWidths.push(String(single_year_widths[mid]));
+      }
+      medianYears.push(String(year_to_find_median_width_for));
+      year_to_find_median_width_for++;
+    }
+
+    // 2) add to data
+    var medianSet = new Object();
+    medianSet.widths = medianWidths;
+    medianSet.years = medianYears;
+    medianSet.name = 'Median';
+    medianSet.color = '#000000'; // black
+    data.push(medianSet);
+
+    return data
+  }
+
   PopoutPlots.prototype.prepData_forPlotting = function (allData) {
     if (Lt.preferences.forwardDirection) { // if measuring forward in time...
       var pts = Lt.data.points;
@@ -3043,70 +3110,7 @@ function Popout(Lt) {
   } else { // spaghetti plot & finding median line
     var shapes = [];
     var colors = [ '#1f78b4' ]; // blue
-
-    // auto spaghetti plot
-    // 1) find median width (across all data) per year
-    var longestDataLength = 0;
-    for (set of allData) {
-      var setLength = set.widths.length;
-      if (setLength > longestDataLength) {
-        longestDataLength = setLength;
-      }
-    }
-
-    var medianYears = [];
-    var medianWidths = [];
-    var year_to_find_median_width_for = Number.MAX_SAFE_INTEGER;
-    var last_year_to_account_for = -1 * (Number.MAX_SAFE_INTEGER);
-
-    for (set of allData) {
-      let years = set.years.filter(Boolean);
-
-      var sets_first_year = parseInt(years[0])
-      if (sets_first_year < year_to_find_median_width_for) { // find oldest (smallest) year in all sets of data
-        year_to_find_median_width_for = sets_first_year;
-      }
-
-      var sets_last_year = parseInt(years[years.length - 1])
-      if (sets_last_year > last_year_to_account_for) { // find most recent (largest) year in all sets of data
-        last_year_to_account_for = sets_last_year;
-      }
-    }
-
-    while (year_to_find_median_width_for <= last_year_to_account_for) {
-      var single_year_widths = []
-      for (set of allData) { // loop through data sets
-        for (let i = 0; i < set.years.length; i++) { // loop through years & widths of each data set
-          if ((set.years.length > 0) && (parseInt(set.years[i]) == year_to_find_median_width_for)) {
-            single_year_widths.push(parseFloat(set.widths[i]));
-          }
-        }
-      }
-
-      single_year_widths.sort( (a, b) => { return a - b} ); // sort into asscending order
-      if (single_year_widths.length == 0) {
-        medianWidths.push('0');
-      } else if (single_year_widths.length % 2 == 0) { // if even length, need to take average of middle values
-        var midUpper = (single_year_widths.length / 2);
-        var midLower = (single_year_widths.length / 2) - 1;
-        var value = String((single_year_widths[midUpper] + single_year_widths[midLower]) / 2);
-        medianWidths.push(value);
-      } else {
-        var mid = Math.floor(single_year_widths.length / 2);
-        medianWidths.push(String(single_year_widths[mid]));
-      }
-      medianYears.push(String(year_to_find_median_width_for));
-      year_to_find_median_width_for++;
-    }
-
-    // 2) add to allData
-    var medianSet = new Object();
-    medianSet.widths = medianWidths;
-    medianSet.years = medianYears;
-    medianSet.name = 'Median';
-    medianSet.color = '#000000'; // black
-    allData.push(medianSet);
-
+    allData = this.median(shapes, colors, allData);
   }
 
     var data = [];
